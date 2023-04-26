1069dbc1ac26c642b232eed152297a808693018d
eonz1 <meonzzz1@gmail.com>
2023-04-26 11:52:15 +0900

    Initial commit

---
+++ src/main/java/com/cgram/prom/PromApplication.java
@@ -0,0 +1,13 @@
+package com.cgram.prom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromApplication.class, args);
+	}
+
+}
