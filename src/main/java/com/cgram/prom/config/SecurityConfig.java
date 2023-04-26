59c18c2bbaacb44ca5e06248b26ab1a6c7409902
chaeyeon <songchaechaes@gmail.com>
2023-04-26 16:09:57 +0900

    Add: Spring Security 설정 파일

---
+++ src/main/java/com/cgram/prom/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cgram.prom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .logout().disable()
+                .httpBasic().disable();
+
+        return http.build();
+    }
+}
