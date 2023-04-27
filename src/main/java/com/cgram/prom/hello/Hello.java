d96e5eb233a929658ccee216a5325ebb6c49ce06
chaeyeon <songchaechaes@gmail.com>
2023-04-27 16:07:47 +0900

    Add: Hello entity

---
+++ src/main/java/com/cgram/prom/hello/Hello.java
@@ -0,0 +1,25 @@
+package com.cgram.prom.hello;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Hello {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Hello(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
