d3db6862e0fa6d2e9eb5308028cc3518698a645f
chaeyeon <songchaechaes@gmail.com>
2023-04-27 16:08:20 +0900

    Add: helloJpa api

--- src/main/java/com/cgram/prom/hello/HelloController.java
+++ src/main/java/com/cgram/prom/hello/HelloController.java
@@ -11,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final HelloMapper helloMapper;
+    private final HelloRepository helloRepository;
 
     @GetMapping("")
     public String hello() {
         return helloMapper.sayHello();
+    }
+
+    @GetMapping("/jpa")
+    public String helloJpa() {
+        Hello hello = helloRepository.findByName("hello world")
+            .orElseGet(() -> new Hello(3L, "hello3"));
+        return hello.getName();
     }
 }
