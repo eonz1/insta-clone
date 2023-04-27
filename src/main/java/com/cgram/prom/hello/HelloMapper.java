f98a8c2f25341d4c83d1e9c8e05a7091b88dcc37
eonz1 <meonzzz1@gmail.com>
2023-04-27 17:03:15 +0900

    Feat: 'halo' insert구문 추가

--- src/main/java/com/cgram/prom/hello/HelloMapper.java
+++ src/main/java/com/cgram/prom/hello/HelloMapper.java
@@ -1,9 +1,13 @@
 package com.cgram.prom.hello;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface HelloMapper {
 
     String sayHello();
+
+    @Insert("insert into hello(name) values('halo')")
+    void addHello();
 }
