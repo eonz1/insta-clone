80ef3360495354c38b1f626c589d56592dd36e80
eonz1 <meonzzz1@gmail.com>
2023-05-03 08:26:31 +0900

    Rename: 도메인형 패키지 구조 변경으로 인한 파일 이동
    config파일은 global 패키지안으로

--- src/main/java/com/cgram/prom/global/config/SecurityConfig.java
+++ src/main/java/com/cgram/prom/global/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.cgram.prom.config;
+package com.cgram.prom.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
