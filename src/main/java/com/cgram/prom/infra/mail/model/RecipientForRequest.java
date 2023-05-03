3578ecc48f51245e44131687f2faf180b5f4228e
eonz1 <meonzzz1@gmail.com>
2023-05-03 17:26:36 +0900

    Feat: 네이버 클라우드 메일 전송 기능 추가

---
+++ src/main/java/com/cgram/prom/infra/mail/model/RecipientForRequest.java
@@ -0,0 +1,48 @@
+package com.cgram.prom.infra.mail.model;
+
+public class RecipientForRequest {
+
+    private String address = null;
+    private String name = null;
+    private String type = "R";
+    private Object parameters = null;
+
+    public RecipientForRequest() {
+    }
+
+    public RecipientForRequest(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Object parameters) {
+        this.parameters = parameters;
+    }
+}
