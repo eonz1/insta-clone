7af59b7a7e38f9fbe0e5557e2dfcf98ccbf9ac01
eonz1 <meonzzz1@gmail.com>
2023-05-10 08:47:25 +0900

    Feat: 비밀번호 변경

--- src/main/java/com/cgram/prom/domain/user/domain/User.java
+++ src/main/java/com/cgram/prom/domain/user/domain/User.java
@@ -33,4 +33,8 @@ public class User {
         this.password = password;
         this.isPresent = isPresent;
     }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 }
