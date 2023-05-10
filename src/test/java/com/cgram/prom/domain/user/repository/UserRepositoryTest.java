c52a3b4da144d25325be09af3c63c3faec646a07
eonz1 <meonzzz1@gmail.com>
2023-05-10 15:18:22 +0900

    Refactor: ActiveProfiles(dev) 추가

--- src/test/java/com/cgram/prom/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/cgram/prom/domain/user/repository/UserRepositoryTest.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @DataJpaTest
+@ActiveProfiles("dev")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
