ed0b46e1c1653e3db9e25f5f42603c180abb5e0d
chaeyeon <songchaechaes@gmail.com>
2023-05-16 08:37:19 +0900

    Feat: JPA Auditing 사용을 위해 어노테이션 추가

--- src/main/java/com/cgram/prom/PromApplication.java
+++ src/main/java/com/cgram/prom/PromApplication.java
@@ -2,12 +2,14 @@ package com.cgram.prom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PromApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PromApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PromApplication.class, args);
+    }
 
 }
