69919b0b39d8aeec6c2c048a4c6ad333fde9a9b4
chaeyeon <songchaechaes@gmail.com>
2023-05-18 14:46:47 +0900

    Feat: Spring Security 인증 테스트를 위한 어노테이션
    커스텀 인증 정보를 받는 테스트를 위해 @WithUserUser 어노테이션 생성

---
+++ src/test/java/com/cgram/prom/global/security/jwt/filter/WithAuthUser.java
@@ -0,0 +1,14 @@
+package com.cgram.prom.global.security.jwt.filter;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithAuthUserSecurityContextFactory.class)
+public @interface WithAuthUser {
+
+    String username();
+
+    String refreshToken();
+}
