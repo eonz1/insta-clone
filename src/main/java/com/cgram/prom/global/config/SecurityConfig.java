637af4f17ac802c1289c6ac5754da369ff8ae5f1
chaeyeon <songchaechaes@gmail.com>
2023-05-24 16:33:52 +0900

    Feat: SecurityConfig 파일에 jwt 설정 수정

--- src/main/java/com/cgram/prom/global/config/SecurityConfig.java
+++ src/main/java/com/cgram/prom/global/config/SecurityConfig.java
@@ -1,8 +1,17 @@
 package com.cgram.prom.global.config;
 
-import com.cgram.prom.global.security.jwt.filter.JwtAuthFilter;
-import com.cgram.prom.global.security.jwt.service.TokenProvider;
+import com.cgram.prom.global.security.jwt.exception.CustomAuthenticationEntryPoint;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,15 +19,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final TokenProvider tokenProvider;
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
 
     @Bean
     public BCryptPasswordEncoder getPasswordEncoder() {
@@ -28,24 +45,48 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf()
+            .ignoringRequestMatchers("/api/v1/**")
+            .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .formLogin().disable()
             .logout().disable()
-            .httpBasic().disable().authorizeHttpRequests()
+            .authorizeHttpRequests()
             .requestMatchers(HttpMethod.POST, "/api/v1/users/**").permitAll()
             .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "/api/v1/verification/**").permitAll()
             .requestMatchers(HttpMethod.POST, "/api/v1/password/code").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/v1/token/reissue").permitAll()
             .requestMatchers(HttpMethod.OPTIONS).permitAll()
             .anyRequest()
             .authenticated()
             .and()
-            .addFilterBefore(new JwtAuthFilter(tokenProvider),
-                UsernamePasswordAuthenticationFilter.class);
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint(objectMapper()))
+                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                .jwt()
+                .decoder(decoder())
+            );
 
         return http.build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public JwtDecoder decoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
