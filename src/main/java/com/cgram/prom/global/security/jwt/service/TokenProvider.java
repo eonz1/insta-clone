73b8a25672dbf2465fb35874e4d07957db77368b
chaeyeon <songchaechaes@gmail.com>
2023-05-24 16:38:16 +0900

    Feat: jwt 토큰 발급, 재발급 새로 구현
    기존의 jwt 토큰 검증 관련 메서드들은 모두 삭제하고 발급, 재발급 메서드만 TokenProvider에서 관리

--- src/main/java/com/cgram/prom/global/security/jwt/service/TokenProvider.java
+++ src/main/java/com/cgram/prom/global/security/jwt/service/TokenProvider.java
@@ -4,20 +4,12 @@ package com.cgram.prom.global.security.jwt.service;
 import com.cgram.prom.domain.user.domain.User;
 import com.cgram.prom.global.security.jwt.domain.RefreshToken;
 import com.cgram.prom.global.security.jwt.domain.Token;
-import com.cgram.prom.global.security.jwt.exception.TokenException;
-import com.cgram.prom.global.security.jwt.exception.TokenExceptionType;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
-import java.util.Date;
-import javax.crypto.SecretKey;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -25,80 +17,45 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TokenProvider {
 
-    private final TokenProperties tokenProperties;
     private final RefreshTokenService refreshTokenService;
-
-    private SecretKey key;
-
-    @PostConstruct
-    protected void init() {
-        key = Keys.hmacShaKeyFor(
-            Decoders.BASE64.decode(tokenProperties.getSecretKey()));
-    }
+    private final JwtEncoder jwtEncoder;
 
     public Token issueToken(User user) {
-        Claims claims = Jwts.claims()
-            .setIssuer("cgram.com");
-        Claims accessClaims = Jwts.claims().setIssuer("cgram.com")
-            .setSubject(user.getId().toString());
+        long accessPeriod = 60L * 10L;
+        long refreshPeriod = 60L * 60L * 24L * 7L;
 
-        long accessPeriod = 1000L * 60L * 10L;
-        long refreshPeriod = 1000L * 60L * 60L * 24L * 7L;
+        JwtClaimsSet accessClaims = JwtClaimsSet.builder()
+            .subject(user.getId().toString())
+            .build();
 
-        String accessToken = generateToken(accessClaims, getExpirationDate(accessPeriod));
-        String refreshToken = generateToken(claims, getExpirationDate(refreshPeriod));
+        JwtClaimsSet refreshClaims = JwtClaimsSet.builder()
+            .subject("")
+            .build();
+
+        String accessToken = generateToken(accessClaims, accessPeriod);
+        String refreshToken = generateToken(refreshClaims, refreshPeriod);
 
         refreshTokenService.save(user, refreshToken);
 
         return new Token(accessToken, refreshToken);
     }
 
-    public Token reIssueToken(String accessToken, String refreshToken) {
+    private String generateToken(JwtClaimsSet claims, long expiredAt) {
+        Instant now = Instant.now();
+        JwtClaimsSet claimsSet = JwtClaimsSet.from(claims)
+            .issuer("cgram.com")
+            .issuedAt(now)
+            .expiresAt(now.plusSeconds(expiredAt))
+
+            .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+
+    public Token reIssueToken(String refreshToken) {
         RefreshToken refreshTokenEntity = refreshTokenService.getRefreshToken(refreshToken);
-
-        String userId = refreshTokenEntity.getUser().getId().toString();
-        String subject = getTokenSubject(accessToken);
-
-        if (!verifyToken(refreshToken) || !subject.equals(userId)) {
-            throw new TokenException(TokenExceptionType.TOKEN_INVALID);
-        }
         refreshTokenService.deleteToken(refreshTokenEntity);
-        
+
         return issueToken(refreshTokenEntity.getUser());
-    }
-
-    public String generateToken(Claims claims, Date expiredAt) {
-        return Jwts.builder()
-            .setHeaderParam("typ", "JWT")
-            .setClaims(claims)
-            .setIssuedAt(new Date())
-            .setExpiration(expiredAt)
-            .signWith(key, SignatureAlgorithm.HS256)
-            .compact();
-    }
-
-    private Date getExpirationDate(long period) {
-        Date now = new Date();
-        return new Date(now.getTime() + period);
-    }
-
-    public String getTokenSubject(String token) {
-        try {
-            return Jwts.parserBuilder().setSigningKey(key).build()
-                .parseClaimsJws(token).getBody().getSubject();
-        } catch (ExpiredJwtException e) {
-            return e.getClaims().getSubject();
-        }
-    }
-
-    public boolean verifyToken(String token) {
-        try {
-            Jwts.parserBuilder()
-                .setSigningKey(key).build()
-                .parseClaimsJws(token);
-            return true;
-        } catch (JwtException e) {
-            return false;
-        }
     }
 }
