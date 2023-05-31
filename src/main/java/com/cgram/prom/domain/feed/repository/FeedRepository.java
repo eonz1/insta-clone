788411d99668a5aeda23e22894feab1cea3cd628
chaeyeon <songchaechaes@gmail.com>
2023-05-31 09:28:40 +0900

    Remove: 안쓰는 메서드 삭제

--- src/main/java/com/cgram/prom/domain/feed/repository/FeedRepository.java
+++ src/main/java/com/cgram/prom/domain/feed/repository/FeedRepository.java
@@ -1,11 +1,9 @@
 package com.cgram.prom.domain.feed.repository;
 
 import com.cgram.prom.domain.feed.domain.Feed;
-import com.cgram.prom.domain.user.domain.User;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedRepository extends JpaRepository<Feed, UUID> {
 
-    Long countByUser(User build);
 }
