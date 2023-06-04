860be31cd9c56b650476ae9a787416492a288d6d
chaeyeon <songchaechaes@gmail.com>
2023-06-04 15:11:16 +0900

    Test: 이미지 resize해서 저장하는 메서드 생성

--- src/test/java/com/cgram/prom/domain/image/service/ImageServiceImplTest.java
+++ src/test/java/com/cgram/prom/domain/image/service/ImageServiceImplTest.java
@@ -1,19 +1,30 @@
 package com.cgram.prom.domain.image.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class ImageServiceImplTest {
+
     private String path;
     private File file;
     private String extension;
     private BufferedImage bufferedImage;
+
+    @InjectMocks
+    ImageServiceImpl imageService;
 
     @BeforeEach
     void Setup() throws IOException {
@@ -25,11 +36,32 @@ class ImageServiceImplTest {
 
     @Test
     void jpg_파일로_변환하여_저장하기() throws IOException {
-        BufferedImage afterImg = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(),
+        BufferedImage afterImg = new BufferedImage(bufferedImage.getWidth(),
+            bufferedImage.getHeight(),
             BufferedImage.TYPE_3BYTE_BGR);
-        afterImg.createGraphics().drawImage(bufferedImage, 0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(),null);
+        afterImg.createGraphics()
+            .drawImage(bufferedImage, 0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(),
+                null);
 
         ImageIO.write(afterImg, "jpg",
             new File(path + File.separator + "convert_mongja.jpg"));
+    }
+
+    @Test
+    @DisplayName("원하는 크기로 이미지 리사이즈해서 prefix 붙여서 저장하기")
+    public void resizeCoverImage() throws Exception {
+        // given
+        int width = 250;
+        int height = 250;
+
+        // when
+        File resizedImage = imageService.resizeImage(file, "cover_", width, height);
+
+        // then
+        Image image = new ImageIcon(resizedImage.getAbsolutePath()).getImage();
+        assertThat(image.getWidth(null)).isEqualTo(width);
+        assertThat(image.getHeight(null)).isEqualTo(height);
+
+        assertThat(resizedImage.getName()).isEqualTo("cover_" + file.getName());
     }
 }
