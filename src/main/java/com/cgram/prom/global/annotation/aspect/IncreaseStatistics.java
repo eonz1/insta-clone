3544641856d850d3b91948fe6338a36b698c8f08
eonz1 <meonzzz1@gmail.com>
2023-06-05 13:45:34 +0900

    Feat: 추가 및 삭제 시 통계반영 aop

---
+++ src/main/java/com/cgram/prom/global/annotation/aspect/IncreaseStatistics.java
@@ -0,0 +1,14 @@
+package com.cgram.prom.global.annotation.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IncreaseStatistics {
+
+    String type();
+    String id();
+}
