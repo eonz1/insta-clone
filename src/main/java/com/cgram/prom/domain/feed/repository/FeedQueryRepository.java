5338d3601ce6e36b57436e1251a197fe4bda8f21
chaeyeon <songchaechaes@gmail.com>
2023-06-08 08:43:00 +0900

    Feat: 팔로잉, 내 프로필로 피드 조회하기

--- src/main/java/com/cgram/prom/domain/feed/repository/FeedQueryRepository.java
+++ src/main/java/com/cgram/prom/domain/feed/repository/FeedQueryRepository.java
@@ -3,7 +3,11 @@ package com.cgram.prom.domain.feed.repository;
 import com.cgram.prom.domain.feed.domain.Feed;
 import com.cgram.prom.domain.feed.domain.QFeed;
 import com.cgram.prom.domain.feed.domain.hashtag.QHashTag;
+import com.cgram.prom.domain.feed.request.GetFeedsServiceDto;
+import com.cgram.prom.domain.following.domain.QFollow;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +19,53 @@ public class FeedQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<Feed> getAllFeedsByHashTag(String hashTag) {
+    public List<Feed> getAllFeedsByHashTag(GetFeedsServiceDto dto, LocalDateTime lastDate) {
         return queryFactory
             .selectFrom(QFeed.feed)
             .join(QFeed.feed.hashTags, QHashTag.hashTag)
-            .on(QHashTag.hashTag.tag.eq(hashTag))
+            .on(QHashTag.hashTag.tag.eq(dto.getTag()))
+            .where(loeFeedId(dto.getCursor()))
+            .on(QFeed.feed.createdAt.gt(lastDate))
+            .orderBy(QFeed.feed.createdAt.desc())
+            .limit(dto.getOffset() + 1)
             .fetch();
     }
 
-    public List<Feed> getAllFeedsByProfiles(List<UUID> profileIds) {
-        return null;
+    public List<Feed> getAllFeedsByMyFollowings(GetFeedsServiceDto dto, LocalDateTime lastDate) {
+        // 내가 팔로우한 사람들
+        return queryFactory
+            .selectFrom(QFeed.feed)
+            .join(QFollow.follow)
+            .on(QFollow.follow.isPresent,
+                QFollow.follow.profileId.id.eq(UUID.fromString(dto.getProfileId())))
+            .on(QFeed.feed.profile.id.eq(QFollow.follow.followedId.id))
+            .on(QFeed.feed.createdAt.gt(lastDate))
+            .where(loeFeedId(dto.getCursor()))
+            .orderBy(QFeed.feed.createdAt.desc())
+            .limit(dto.getOffset() + 1)
+            .fetchJoin()
+            .fetch();
+    }
+
+    public List<Feed> getAllFeedsByMyProfile(GetFeedsServiceDto dto, LocalDateTime lastDate) {
+        return queryFactory
+            .selectFrom(QFeed.feed)
+            .where(
+                QFeed.feed.isPresent,
+                QFeed.feed.profile.id.eq(UUID.fromString(dto.getProfileId())),
+                loeFeedId(dto.getCursor()),
+                QFeed.feed.createdAt.gt(lastDate)
+            )
+            .orderBy(QFeed.feed.createdAt.desc())
+            .limit(dto.getOffset() + 1)
+            .fetch();
+    }
+
+    private BooleanExpression loeFeedId(String feedId) {
+        if (feedId == null) {
+            return null;
+        }
+
+        return QFeed.feed.id.loe(UUID.fromString(feedId));
     }
 }
