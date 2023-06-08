ca9e4d1e84c89cdac3766b413ce14ee989a04758
eonz1 <meonzzz1@gmail.com>
2023-06-08 17:24:32 +0900

    Test: 피드아이디로 댓글 최상위 3개 조회하기 querydsl로 변경
    피드아이디 리스트로 받아서 where절 조건에 in으로 걸어야

--- src/test/java/com/cgram/prom/domain/comment/repository/CommentRepositoryTest.java
+++ src/test/java/com/cgram/prom/domain/comment/repository/CommentRepositoryTest.java
@@ -6,6 +6,8 @@ import com.cgram.prom.domain.comment.domain.Comment;
 import com.cgram.prom.domain.feed.domain.Feed;
 import com.cgram.prom.domain.feed.repository.FeedRepository;
 import com.cgram.prom.global.config.JpaAuditingConfig;
+import com.cgram.prom.global.config.QuerydslTestConfig;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.assertj.core.api.Assertions;
@@ -20,11 +22,14 @@ import org.springframework.test.context.ActiveProfiles;
 @DataJpaTest
 @ActiveProfiles("dev")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import({JpaAuditingConfig.class})
+@Import({QuerydslTestConfig.class, CommentQueryRepository.class, JpaAuditingConfig.class})
 class CommentRepositoryTest {
 
     @Autowired
     private CommentRepository commentRepository;
+
+    @Autowired
+    private CommentQueryRepository commentQueryRepository;
 
     @Autowired
     private FeedRepository feedRepository;
@@ -49,14 +54,19 @@ class CommentRepositoryTest {
     public void findTop3ByFeedIdAndIsPresent() {
         // given
         Feed feed = feedRepository.save(Feed.builder().build());
+        Feed feed2 = feedRepository.save(Feed.builder().build());
         commentRepository.save(Comment.builder().feed(feed).content("코멘트1").build());
         commentRepository.save(Comment.builder().feed(feed).content("코멘트2").build());
         commentRepository.save(Comment.builder().feed(feed).content("코멘트3").build());
         commentRepository.save(Comment.builder().feed(feed).content("코멘트4").build());
         commentRepository.save(Comment.builder().feed(feed).content("코멘트5").build());
 
+        List<UUID> feedUUIDs = new ArrayList<>();
+        feedUUIDs.add(feed.getId());
+        feedUUIDs.add(feed2.getId());
+
         // when
-        List<Comment> commentList = commentRepository.findTop3ByFeedIdAndIsPresentOrderByCreatedAtDesc(feed.getId(), true);
+        List<Comment> commentList = commentQueryRepository.getAllCommentsByFeedIds(feedUUIDs, 3);
 
         // then
         Assertions.assertThat(commentList.size()).isEqualTo(3);
