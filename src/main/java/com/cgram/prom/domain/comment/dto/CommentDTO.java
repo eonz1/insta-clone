10b0adf4d7746a55594c4fe777206aeb241646a3
chaeyeon <songchaechaes@gmail.com>
2023-06-14 15:56:58 +0900

    Feat: 테스트 위해서 FeedDTO, CommentDTO에 builder 추가
    QueryRepository에서 Projections 이용해서 바로 생성되기 때문에 builder가 필요없었으나, 테스트 시 dto 임의 생성이 필요해서 추가

--- src/main/java/com/cgram/prom/domain/comment/dto/CommentDTO.java
+++ src/main/java/com/cgram/prom/domain/comment/dto/CommentDTO.java
@@ -1,9 +1,8 @@
 package com.cgram.prom.domain.comment.dto;
 
-import com.cgram.prom.domain.feed.domain.Feed;
-import com.cgram.prom.domain.profile.domain.Profile;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,4 +31,22 @@ public class CommentDTO {
     private boolean isPresent;
 
     private int likesCount;
+
+    @Builder
+    public CommentDTO(UUID id, UUID feedId, UUID userId, UUID profileImageId,
+        String profileImagePath,
+        String userEmail, String content, LocalDateTime createdAt, LocalDateTime modifiedAt,
+        boolean isPresent, int likesCount) {
+        this.id = id;
+        this.feedId = feedId;
+        this.userId = userId;
+        this.profileImageId = profileImageId;
+        this.profileImagePath = profileImagePath;
+        this.userEmail = userEmail;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.isPresent = isPresent;
+        this.likesCount = likesCount;
+    }
 }
--- src/main/java/com/cgram/prom/domain/feed/dto/FeedDTO.java
+++ src/main/java/com/cgram/prom/domain/feed/dto/FeedDTO.java
@@ -2,7 +2,7 @@ package com.cgram.prom.domain.feed.dto;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -26,4 +26,20 @@ public class FeedDTO {
 
     private int commentCount;
     private int likesCount;
+
+    @Builder
+    public FeedDTO(UUID id, UUID userId, String email, UUID profileImageId, String profileImagePath,
+        String content, LocalDateTime createdAt, LocalDateTime modifiedAt, int commentCount,
+        int likesCount) {
+        this.id = id;
+        this.userId = userId;
+        this.email = email;
+        this.profileImageId = profileImageId;
+        this.profileImagePath = profileImagePath;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.commentCount = commentCount;
+        this.likesCount = likesCount;
+    }
 }
