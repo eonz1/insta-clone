2679b21f8cf9626c195301e12fb8e47b9fc4a7cc
chaeyeon <songchaechaes@gmail.com>
2023-06-14 09:18:13 +0900

    Feat: CommentResponse에 기본 빌더 패턴 생성자 외에 CommentDto로 생성하는 생성자 추가

--- src/main/java/com/cgram/prom/domain/comment/response/CommentResponse.java
+++ src/main/java/com/cgram/prom/domain/comment/response/CommentResponse.java
@@ -1,13 +1,14 @@
 package com.cgram.prom.domain.comment.response;
 
+import com.cgram.prom.domain.comment.dto.CommentDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.io.File;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class CommentResponse {
 
     private UUID id;
@@ -27,4 +28,37 @@ public class CommentResponse {
     private LocalDateTime modifiedAt;
 
     private long likes;
+
+    @Builder
+    public CommentResponse(UUID id, String content, UUID userId, String userEmail,
+        String profileImagePath, LocalDateTime createdAt, LocalDateTime modifiedAt, long likes) {
+        this.id = id;
+        this.content = content;
+        this.userId = userId;
+        this.userEmail = userEmail;
+        this.profileImagePath = profileImagePath;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.likes = likes;
+    }
+
+    public CommentResponse(CommentDTO dto) {
+        this.id = dto.getId();
+        this.content = dto.getContent();
+        this.userId = dto.getUserId();
+        this.userEmail = dto.getUserEmail();
+        this.createdAt = dto.getCreatedAt();
+        this.modifiedAt = dto.getModifiedAt();
+        this.likes = dto.getLikesCount();
+
+        setProfileImagePath(dto.getProfileImageId(), dto.getProfileImagePath());
+    }
+
+    private void setProfileImagePath(UUID imageId, String path) {
+        if (imageId == null) {
+            return;
+        }
+
+        this.profileImagePath = path + File.separator + imageId + ".jpg";
+    }
 }
