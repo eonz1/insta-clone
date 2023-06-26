5964ef3cb271abfa045ff7150c30d20cf64b82a7
chaeyeon <songchaechaes@gmail.com>
2023-06-27 08:48:55 +0900

    Test: 팔로잉, 언팔로잉 중복해서 실행하는 경우 통계 테이블 업데이트 되는 문제
    이미 팔로잉, 언팔로잉 되어 있는 경우 바로 리턴하도록 수정

--- src/test/java/com/cgram/prom/domain/following/service/FollowServiceImplTest.java
+++ src/test/java/com/cgram/prom/domain/following/service/FollowServiceImplTest.java
@@ -10,6 +10,7 @@ import com.cgram.prom.domain.following.domain.Follow;
 import com.cgram.prom.domain.following.domain.FollowId;
 import com.cgram.prom.domain.following.repository.FollowRepository;
 import com.cgram.prom.domain.profile.domain.Profile;
+import com.cgram.prom.domain.statistics.enums.StatisticType;
 import com.cgram.prom.domain.statistics.service.StatisticsServiceImpl;
 import com.cgram.prom.domain.user.domain.User;
 import java.util.Optional;
@@ -41,28 +42,33 @@ class FollowServiceImplTest {
         User user = User.builder().id(UUID.randomUUID()).build();
         User followedUser = User.builder().id(UUID.randomUUID()).build();
 
-        Profile followedUserProfile = Profile.builder().user(followedUser).build();
-        Profile userProfile = Profile.builder().user(user).build();
+        Profile followedUserProfile = Profile.builder().id(UUID.randomUUID()).user(followedUser)
+            .build();
+        Profile userProfile = Profile.builder().id(UUID.randomUUID()).user(user).build();
 
-        when(followRepository.findById(any(FollowId.class))).thenReturn(
-            Optional.empty());
+        when(followRepository.findById(any(FollowId.class))).thenReturn(Optional.empty());
 
         // when
         followService.follow(followedUserProfile, userProfile);
 
         // then
         verify(followRepository, times(1)).save(any(Follow.class));
+        verify(statisticsService, times(1)).updateStatistics(followedUserProfile.getId(),
+            StatisticType.FOLLOWER.label(), 1);
+        verify(statisticsService, times(1)).updateStatistics(userProfile.getId(),
+            StatisticType.FOLLOWING.label(), 1);
     }
 
     @Test
-    @DisplayName("이미 팔로잉 기록이 있으면 팔로잉 상태만 활성화시킨다.")
+    @DisplayName("팔로잉 했다가 취소한 적이 있으면 팔로잉 상태만 활성화시킨다.")
     public void updateFollowingStatus() throws Exception {
         // given
         User user = User.builder().id(UUID.randomUUID()).build();
         User followedUser = User.builder().id(UUID.randomUUID()).build();
 
-        Profile followedUserProfile = Profile.builder().user(followedUser).build();
-        Profile userProfile = Profile.builder().user(user).build();
+        Profile followedUserProfile = Profile.builder().id(UUID.randomUUID()).user(followedUser)
+            .build();
+        Profile userProfile = Profile.builder().id(UUID.randomUUID()).user(user).build();
 
         Optional<Follow> follow = Optional.of(Follow.builder()
             .profileId(userProfile)
@@ -77,6 +83,10 @@ class FollowServiceImplTest {
         // then
         verify(followRepository, never()).save(any(Follow.class));
         Assertions.assertThat(follow.get().isPresent()).isEqualTo(true);
+        verify(statisticsService, times(1)).updateStatistics(followedUserProfile.getId(),
+            StatisticType.FOLLOWER.label(), 1);
+        verify(statisticsService, times(1)).updateStatistics(userProfile.getId(),
+            StatisticType.FOLLOWING.label(), 1);
     }
 
     @Test
@@ -86,8 +96,9 @@ class FollowServiceImplTest {
         User user = User.builder().id(UUID.randomUUID()).build();
         User followedUser = User.builder().id(UUID.randomUUID()).build();
 
-        Profile followedUserProfile = Profile.builder().user(followedUser).build();
-        Profile userProfile = Profile.builder().user(user).build();
+        Profile followedUserProfile = Profile.builder().id(UUID.randomUUID()).user(followedUser)
+            .build();
+        Profile userProfile = Profile.builder().id(UUID.randomUUID()).user(user).build();
 
         Optional<Follow> follow = Optional.of(Follow.builder()
             .profileId(userProfile)
@@ -103,5 +114,68 @@ class FollowServiceImplTest {
 
         // then
         Assertions.assertThat(follow.get().isPresent()).isEqualTo(false);
+        verify(statisticsService, times(1)).updateStatistics(followedUserProfile.getId(),
+            StatisticType.FOLLOWER.label(), -1);
+        verify(statisticsService, times(1)).updateStatistics(userProfile.getId(),
+            StatisticType.FOLLOWING.label(), -1);
+    }
+
+    @Test
+    @DisplayName("이미 팔로잉 되어 있으면 다시 팔로잉 하지 않는다")
+    public void followUserDuplicate() throws Exception {
+        // given
+        User user = User.builder().id(UUID.randomUUID()).build();
+        User followedUser = User.builder().id(UUID.randomUUID()).build();
+
+        Profile followedUserProfile = Profile.builder().id(UUID.randomUUID()).user(followedUser)
+            .build();
+        Profile userProfile = Profile.builder().id(UUID.randomUUID()).user(user).build();
+
+        Optional<Follow> follow = Optional.of(Follow.builder()
+            .profileId(userProfile)
+            .followedId(followedUserProfile)
+            .isPresent(true)
+            .build());
+        when(followRepository.findById(any(FollowId.class))).thenReturn(follow);
+
+        // when
+        followService.follow(followedUserProfile, userProfile);
+
+        // then
+        verify(followRepository, never()).save(any(Follow.class));
+        verify(statisticsService, times(0)).updateStatistics(followedUserProfile.getId(),
+            StatisticType.FOLLOWER.label(), 1);
+        verify(statisticsService, times(0)).updateStatistics(userProfile.getId(),
+            StatisticType.FOLLOWING.label(), 1);
+    }
+
+    @Test
+    @DisplayName("이미 언팔로잉 되어 있으면 다시 언팔로잉 하지 않는다")
+    public void unfollowUserDuplicate() throws Exception {
+        // given
+        User user = User.builder().id(UUID.randomUUID()).build();
+        User followedUser = User.builder().id(UUID.randomUUID()).build();
+
+        Profile followedUserProfile = Profile.builder().id(UUID.randomUUID()).user(followedUser)
+            .build();
+        Profile userProfile = Profile.builder().id(UUID.randomUUID()).user(user).build();
+
+        Optional<Follow> follow = Optional.of(Follow.builder()
+            .profileId(userProfile)
+            .followedId(followedUserProfile)
+            .isPresent(false)
+            .build());
+
+        when(followRepository.findById(any(FollowId.class))).thenReturn(
+            follow);
+
+        // when
+        followService.unfollow(followedUserProfile, userProfile);
+
+        // then
+        verify(statisticsService, times(0)).updateStatistics(followedUserProfile.getId(),
+            StatisticType.FOLLOWER.label(), -1);
+        verify(statisticsService, times(0)).updateStatistics(userProfile.getId(),
+            StatisticType.FOLLOWING.label(), -1);
     }
 }
