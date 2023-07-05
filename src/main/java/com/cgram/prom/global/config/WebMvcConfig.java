e42b88836af968b114751d8bd4978e68d4d9516a
eonz1 <meonzzz1@gmail.com>
2023-07-05 10:59:52 +0900

    Fix: addResourceHandler의 addResourceLocations 수정
    imageDirectory와 일치하게 수정

--- src/main/java/com/cgram/prom/global/config/WebMvcConfig.java
+++ src/main/java/com/cgram/prom/global/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/image/**")
-            .addResourceLocations("classpath:/static/")
+            .addResourceLocations("file:/prom/")
             .setCachePeriod(20);
     }
 }
